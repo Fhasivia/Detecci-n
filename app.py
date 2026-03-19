d9a121e58ada7822613cc4caef98eccd21c33e45
Fhasivia <lauramartinezalzate5550@gmail.com>
2026-03-19 13:56:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,14 @@ if img_file_buffer is not None:
     # run the inference
     prediction = model.predict(data)
     print(prediction)
-    if prediction[0][0]>0.5:
-      st.header('Izquierda, con Probabilidad: '+str( prediction[0][0]) )
-    if prediction[0][1]>0.5:
-      st.header('Arriba, con Probabilidad: '+str( prediction[0][1]))
+    if prediction[0][0]>0.7:
+      st.header('Hola Laura, con Probabilidad: '+str( prediction[0][0]) )
+    if prediction[0][1]>0.7:
+      st.header('Adios Laura, con Probabilidad: '+str( prediction[0][1]))
+    if prediction[0][2]>0.7:
+      st.header('Hola Monica, con Probabilidad: '+str( prediction[0][2]))
+    if prediction[0][3]>0.7:
+      st.header('Hola Ana, con Probabilidad: '+str( prediction[0][3]))
     #if prediction[0][2]>0.5:
     # st.header('Derecha, con Probabilidad: '+str( prediction[0][2]))
 
